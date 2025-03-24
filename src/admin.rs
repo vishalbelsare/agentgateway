9bef03abbe0fdf7ffc5dc0b736dd0d5e5a8c6948
Eitan Yarmush <eitan.yarmush@solo.io>
2025-03-24 20:49:10 +0000

    add listeners

--- src/admin.rs
+++ src/admin.rs
@@ -32,6 +32,7 @@ impl App {
 		Router::new()
 			.route("/targets", get(targets_handler))
 			.route("/rbac", get(rbac_handler))
+			.route("/listeners", get(listener_handler))
 			.with_state(self.clone())
 	}
 }
@@ -46,4 +47,10 @@ async fn rbac_handler(State(app): State<App>) -> Result<String, StatusCode> {
 	let rbac = app.state.read().unwrap().policies.clone();
 	let json_rbac = serde_json::to_string(&rbac).unwrap();
 	Ok(json_rbac)
+}
+
+async fn listener_handler(State(app): State<App>) -> Result<String, StatusCode> {
+	let listener = app.state.read().unwrap().listener.clone();
+	let json_listener = serde_json::to_string(&listener).unwrap();
+	Ok(json_listener)
 }
