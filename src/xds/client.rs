63462d992d93bd9760087cfc2fc989825bf84409
Yuval Kohavi <yuval.kohavi@gmail.com>
2025-03-24 13:55:49 -0400

    add role metadata

--- src/xds/client.rs
+++ src/xds/client.rs
@@ -47,6 +47,7 @@ const DEFAULT_IP: &str = "1.1.1.1";
 const POD_NAME: &str = "POD_NAME";
 const POD_NAMESPACE: &str = "POD_NAMESPACE";
 const NODE_NAME: &str = "NODE_NAME";
+const ROLE: &str = "role";
 const NAME: &str = "NAME";
 const NAMESPACE: &str = "NAMESPACE";
 const EMPTY_STR: &str = "";
@@ -331,17 +332,19 @@ impl Config {
 	fn node(&self) -> Node {
 		let ip = std::env::var(INSTANCE_IP);
 		let ip = ip.as_deref().unwrap_or(DEFAULT_IP);
+		let role = std::env::var(ROLE).unwrap_or_default();
 		let pod_name = std::env::var(POD_NAME);
 		let pod_name = pod_name.as_deref().unwrap_or(EMPTY_STR);
 		let ns = std::env::var(POD_NAMESPACE);
 		let ns = ns.as_deref().unwrap_or(EMPTY_STR);
 		let node_name = std::env::var(NODE_NAME);
 		let node_name = node_name.as_deref().unwrap_or(EMPTY_STR);
-		let mut metadata = Self::build_struct([
+		let metadata = Self::build_struct([
 			(NAME, pod_name),
 			(NAMESPACE, ns),
 			(INSTANCE_IPS, ip),
 			(NODE_NAME, node_name),
+			(ROLE, &role),
 		]);
 
 		Node {
